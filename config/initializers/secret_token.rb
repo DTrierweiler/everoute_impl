f78434378de93dba580e236536fbb06aa8686748
Daniel Trierweiler <trierweiler@dievision.de>
2011-12-28 12:28:32 +0100

    Initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EverouteImpl::Application.config.secret_token = '5a19ec49dfdaa854e966e2d7dabc0dbdb586219f5ccddc863cec6f2c3a58778db95405a1d4dbe9ec96b5f8ff26ff155ab274808a271e12ea8bf5f469992ad2d4'
